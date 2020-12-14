f514b48d775eacdff28fb3a242a530ecd67e58de
Pedro Jardim <jardim.c.pedro@gmail.com>
2020-12-14 09:56:40 -0300

    mudancas na home

--- lib/paginas/home.dart
+++ lib/paginas/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:tcc/widgets/cardsalas.dart';
+import '../widgets/drawer.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({Key key}) : super(key: key);
@@ -11,8 +13,21 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Center(
-        child: Text("Pagina inicial"),
+      child: Scaffold(
+        drawer: Drawer(child: drawerCatalogo()),
+        appBar: AppBar(
+          title: Text("Smart Campus FEB UNESP Bauru"),
+          centerTitle: true,
+        ),
+        body: Container(
+          child: ListView(
+            //TODO: trocar pra builder pegando do servidor os dados do servidor
+            children: [
+              cardsalas("Sala 4"),
+              cardsalas("Sala 7"),
+            ],
+          ),
+        ),
       ),
     );
   }
